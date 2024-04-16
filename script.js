4e8701f7c2013531969104902cba6f97c96effde
Shashank Sharma <shashanksharma8868@gmail.com>
2024-04-16 22:38:14 +0530

    dom project

---
+++ script.js
@@ -0,0 +1,13 @@
+const div = document.querySelector(".container");
+const para = document.querySelector(".text");
+
+div.style.backgroundColor = "yellow";
+div.style.width = "320px";
+div.style.height = "220px";
+div.style.marginTop = "1rem";
+div.style.marginLeft = "2rem";
+div.style.padding = "0.5rem";
+
+para.style.color = "blue";
+para.style.fontWeight = "bold";
+para.style.fontSize = "1.1rem";
